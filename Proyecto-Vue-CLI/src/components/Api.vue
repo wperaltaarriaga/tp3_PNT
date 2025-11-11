474dc7100c943331224df1b40a1b2e1cd75b596b
Wanda <42821432@comunidadort.edu.ar>
2025-11-11 20:13:28 -0300

    ..

--- Proyecto-Vue-CLI/src/components/Api.vue
+++ Proyecto-Vue-CLI/src/components/Api.vue
@@ -5,171 +5,86 @@
             </div>
             <div class="card-body">
 
-                <form @submit.prevent="enviar"> <!-- formulario de ingreso de productos -->
+                <form @submit.prevent="enviar"> 
 
                   <!-- campo nombre -->
                    <div class="form-group">
                       <label for="nombre">nombre</label>
                       <input type="text" id="nombre" class="form-control mb-2" 
-                        v-model.trim="producto.nombre"
-                        @input="productoDirty.nombre = true"  
+                        v-model.trim="dato.nombre"
+                        @input="datoDirty.nombre = true"  
                       /> <!-- Escucha el evento input (cuando el usuario escribe algo) y marca productoDirty.nombre como true. Esto indica que el campo ha sido modificado. -->
                       
-                      <div  v-if="!producto.nombre && productoDirty.nombre" class="alert alert-danger"> <!-- cartel validacion -->
-                        Campo requerido
+                      <div  v-if="errorNombre.mostrar" class="alert alert-danger"> <!-- cartel validacion -->
+                        {{ errorNombre.mensaje }}
                       </div>
-                      <!-- Este bloque muestra un mensaje de validación si se cumplen las siguientes condiciones:
-                        !producto.nombre: El campo producto.nombre está vacío.
-                        productoDirty.nombre: El usuario ya interactuó con el campo (es decir, lo modificó al menos una vez). -->
                    </div>
 
-                   <!-- campo precio -->
+                   <!-- campo edad -->
                    <div class="form-group">
-                    <label for="precio">precio</label>
-                    <input type="number" id="precio" class="form-control mb-2" 
-                      v-model.number="producto.precio"
-                      @input="productoDirty.precio = true"  
-                    />   
-                    <!-- cartel validacion -->                   
-                    <div  v-if="!producto.precio && productoDirty.precio" class="alert alert-danger">
-                      Campo requerido
+                    <label for="edad">edad</label>
+                    <input type="number" id="edad" class="form-control mb-2" 
+                      v-model.number="dato.edad"
+                      @input="datoDirty.edad = true"  
+                    />                     
+                    <div  v-if="errorEdad.mostrar" class="alert alert-danger">
+                      {{errorEdad.mensaje}} 
                     </div>                   
                    </div>
 
-                  <!-- campo stock -->
+
+<!--                   campo email
                   <div class="form-group">
-                    <label for="stock">stock</label>
-                    <input type="number" id="stock" class="form-control mb-2" 
-                      v-model.number="producto.stock"
-                      @input="productoDirty.stock = true"  
-                    />   
-                    <!-- cartel validacion -->                   
-                    <div  v-if="!producto.stock && productoDirty.stock" class="alert alert-danger"> 
+                    <label for="email">email</label>
+                    <input type="text" id="email" class="form-control mb-2" 
+                      v-model.trim="dato.email"
+                      @input="datoDirty.email = true"  
+                    />                       
+                    <div  v-if="!dato.email && datoDirty.email" class="alert alert-danger">
                       Campo requerido
                     </div>                   
-                  </div>
+                   </div>           -->
 
-                  <!-- campo marca -->
-                  <div class="form-group">
-                    <label for="marca">marca</label>
-                    <input type="text" id="marca" class="form-control mb-2" 
-                      v-model.trim="producto.marca"
-                      @input="productoDirty.marca = true"  
-                    />   
-                    <!-- cartel validacion -->                   
-                    <div  v-if="!producto.marca && productoDirty.marca" class="alert alert-danger">
-                      Campo requerido
-                    </div>                   
-                   </div>
-
-                  <!-- campo categoria -->
-                  <div class="form-group">
-                    <label for="categoria">categoria</label>
-                    <input type="text" id="categoria" class="form-control mb-2" 
-                      v-model.trim="producto.categoria"
-                      @input="productoDirty.categoria = true"  
-                    />   
-                    <!-- cartel validacion -->                   
-                    <div  v-if="!producto.categoria && productoDirty.categoria" class="alert alert-danger">
-                      Campo requerido
-                    </div>                   
-                  </div>
-
-                  <!-- campo detalles -->
-                  <div class="form-group">
-                    <label for="detalles">detalles</label>
-                    <input type="text" id="detalles" class="form-control mb-2" 
-                      v-model.trim="producto.detalles"
-                      @input="productoDirty.detalles = true"  
-                    />   
-                    <!-- cartel validacion -->                   
-                    <div  v-if="!producto.detalles && productoDirty.detalles" class="alert alert-danger">
-                      Campo requerido
-                    </div>                   
-                  </div>
-
-                  <!-- campo foto -->
-                  <div class="form-group">
-                    <label for="foto">foto</label>
-                    <input type="text" id="foto" class="form-control mb-2" 
-                      v-model.trim="producto.foto"
-                      @input="productoDirty.foto = true"  
-                    />   
-                    <!-- cartel validacion -->                   
-                    <div  v-if="!producto.foto && productoDirty.foto" class="alert alert-danger">
-                      Campo requerido
-                    </div>                   
-                  </div>
-
-                  <!-- campo envio -->
-                  <div class="form-check my-3">
-                    <input type="checkbox" id="envio" class="form-check-input" 
-                      v-model.trim="producto.envio"
-                    /> 
-                    <label for="envio">envio</label>                  
-                  </div>
-
+                  
                    <!-- boton de envio -->
-                  <button  class="btn btn-success mt-1" :disabled="algunCampoNoValido" > Enviar </button>
+                   <button class="btn btn-success mt-1" :disabled="estadoBotonDeshabilitado()"> Enviar </button>
                    
                 </form>
 
 
               <button class="btn-primary my-3 me-2" @click="obtener"> OBTENER </button> <!-- boton para obtener datos. Vinculo el evento cliclk con la funcion -->
-              <button class="btn-primary my-3 me-2" @click="productos=[]"> BORRAR </button> 
+              <button class="btn-primary my-3 me-2" @click="datos=[]"> BORRAR </button> 
               
-              <!-- 
-              "nombre": "Ball",
-              "precio": "157.99",
-              "stock": 57,
-              "marca": "Computers",
-              "categoria": "Handmade Metal Chips",
-              "detalles": "Savor the savory essence in our Ball, designed for glum culinary adventures",
-              "foto": "https://avatars.githubusercontent.com/u/56204506",
-              "envio": false,
-              "id": "1"
-               -->
-              <div v-if="productos.length"> 
+
+              <div v-if="datos.length"> 
                 <div class="table-responsive">
                   <table class="table table-dark">
                     <thead>
                       <tr>
                         <th>id</th>
                         <th>nombre</th>
-                        <th>precio</th>
-                        <th>stock</th>
-                        <th>marca</th>
-                        <th>categoría</th>
-                        <th>detalles</th>
-                        <th>foto</th>
-                        <th>envío</th>
+                        <th>edad</th>
+                        <th>e-mail</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr v-for="(producto, index) in productos" :key="index">
-                        <td>{{ producto.id}}</td>
-                        <td>{{ producto.nombre }}</td>
-                        <td>{{ producto.precio }}</td>
-                        <td>{{ producto.stock }}</td>
-                        <td>{{producto.marca}}</td>
-                        <td>{{producto.categoria}}</td>
-                        <td>{{producto.detalles}}</td>
-                        <td>
-                          <img :src="producto.foto" height="100" alt="Foto Producto"/>
-                        </td>
-                        <td>{{producto.envio? 'Si' : 'No'}}</td>                        
+                      <tr v-for="(dato, index) in datos" :key="index">
+                        <td>{{ dato.id}}</td>
+                        <td>{{ dato.nombre }}</td>
+                        <td>{{ dato.edad }}</td>
+                        <td>{{ dato.email }}</td>                     
                       </tr>
                     </tbody>
                   </table>
                 </div>
               </div>
-              <h4 v-else class="alert alert-info"> Pedir los productos a traves del boton Obtener</h4>
+              <h4 v-else class="alert alert-info"> Pedir los datos a traves del boton Obtener</h4>
             </div>
         </section>
     </template>
 
 <script>
-import ServicioProductos from '@/servicios/productos.js';// importo el servicio de productos;
+import ServicioProductos from '@/servicios/productos.js';// importo el servicio de datos;
 export default {
   name: 'Api', 
 
@@ -179,17 +94,43 @@ export default {
   },
   data() {
     return {
-      servicioProductos: new ServicioProductos(), // servicio de productos
-      productos:[],// array para almacenar los posts obtenidos
-      producto: this.iniForm(),
-      productoDirty: this.iniForm(),
+      servicioProductos: new ServicioProductos(), // servicio de datos
+      datos:[],// array para almacenar los posts obtenidos
+      dato: this.iniForm(),
+      datoDirty: this.iniForm(),
     };
   },
   computed: { 
-    algunCampoNoValido(){
-      return Object.entries(this.producto).filter(e => e[0]!='envio' ? !e[1]: false).length  // devuelve true si algun campo (excepto envio) es invalido (null, undefined, vacio
-     // return !this.producto.nombre || !this.producto.precio || !this.producto.stock || !this.producto.marca || !this.producto.categoria || !this.producto.detalles || !this.producto.foto || this.producto.envio === null
-    }
+    estadoBotonDeshabilitado(){
+        return !this.errorNombre().ok || !this.errorEdad().ok;
+      },
+    errorNombre(){
+        let mensaje = '';
+        let nombre = this.dato.nombre;
+        if(!nombre) mensaje = 'CAMPO REQUERIDO';
+        else if(nombre.length <= 3) mensaje = 'Este campo debe poseer como minimo 3 caracteres';
+        else if(nombre.length > 10) mensaje = 'Este campo debe poseer como maximo 10 caracteres';
+        else if(nombre.includes(' ')) mensaje = 'El nombre no puede contener espacios intermedios';
+        return{
+          mensaje: mensaje,
+          mostrar: mensaje !== '' && this.datoDirty.nombre,
+          ok: mensaje == '',
+        }
+      },
+
+      errorEdad(){
+        let mensaje = '';
+        let edad = this.dato.edad;
+        if(!edad) mensaje = 'CAMPO REQUERIDO';
+        else if(edad < 18) mensaje = 'Debe ser mayor a 18 años';
+        else if(edad > 120) mensaje = 'Debe ser menor a 120 años';
+        return{
+          mensaje: mensaje,
+          mostrar: mensaje !== '' && this.datoDirty.edad,
+          ok: mensaje == '',
+        }
+      },
+
   },
   watch: {
   },
@@ -197,39 +138,34 @@ export default {
     iniForm(){
       return{
           nombre: null,
-          precio: null,
-          stock: null,
-          marca: null,
-          categoria: null,
-          detalles: null,
-          foto: null,
-          envio: false,
+          edad: null,
+          email: null,
         }
     },
 
 
-    /* -------- Metodos para consumir la API rest full ------------*/
+    /* -------- Metodos para consumir la API rest full ----------*/
 
     //GET 
     async obtener(){
-      this.productos = await this.servicioProductos.getAll() // llamo al metodo obtenerProductos del servicio
-      console.log(this.productos);
+      this.datos = await this.servicioProductos.getAll() // llamo al metodo obtenerProductos del servicio
+      console.log(this.datos);
     },
 
     //POST
     async enviar(){
-      const producto = {...this.producto} // clono el objeto producto para no mutar el original
-      console.log('Producto a enviar:', producto);
+      const dato = {...this.dato} // clono el objeto producto para no mutar el original
+      console.log('dato a enviar:', dato);
 
       //agrego producto en el recurso remoto
-      const productoGuardado = await this.servicioProductos.post(producto); // llamo al metodo post del servicio para guardar el producto
-      console.log('Producto guardado:', productoGuardado);
+      const datoGuardado = await this.servicioProductos.post(dato); // llamo al metodo post del servicio para guardar el producto
+      console.log('dato guardado:', datoGuardado);
 
       //agrego producto en la lista local
-      this.productos.push(productoGuardado);  
+      this.datos.push(datoGuardado);  
 
-      this.producto = this.iniForm(); // reinicio el formulario
-      this.productoDirty = this.iniForm(); // reinicio el estado dirty del formulario
+      this.dato = this.iniForm(); // reinicio el formulario
+      this.datoDirty = this.iniForm(); // reinicio el estado dirty del formulario
     },
 
     //PUT
--- Proyecto-Vue-CLI/src/servicios/productos.js
+++ Proyecto-Vue-CLI/src/servicios/productos.js
@@ -3,24 +3,24 @@ import axios from 'axios'
 class ServicioProductos {
     #url = ''
     constructor() {
-        this.#url = 'https://6910ac527686c0e9c20b576b.mockapi.io/api/productos';
+        this.#url = 'https://6910ac527686c0e9c20b576b.mockapi.io/api/APItp3';
     }
 
     //GET
     getAll = async () => {
         try {
-            const { data: productos } = await axios.get(this.#url);
-            return productos;
+            const { data: datos } = await axios.get(this.#url);
+            return datos;
         } catch (error) {
             console.log('Error productos GET', error.message);
         }
     }
 
     //POST
-    post = async (producto) => {
+    post = async (datos) => {
         try {
-            const { data: productoGuardado } = await axios.post(this.#url, producto);
-            return productoGuardado;
+            const { data: datoGuardado } = await axios.post(this.#url, producto);
+            return datoGuardado;
         } catch (error) {
             console.log('Error productos POST', error.message);
         }
