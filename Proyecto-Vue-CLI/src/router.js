343a3e07f6206a8e8ae67cd45495fdd2ef81dd77
Wanda <42821432@comunidadort.edu.ar>
2025-11-11 19:28:02 -0300

    ..

--- Proyecto-Vue-CLI/src/router.js
+++ Proyecto-Vue-CLI/src/router.js
@@ -1,17 +1,12 @@
 import { createRouter, createWebHistory} from "vue-router";
-import Http from "./components/Http.vue";
 import Api from "./components/Api.vue";
 
 const routes =[
 
     {path: '/', component: Api},  
-
-
-    {path: '/http', component: Http},
     {path: '/api', component: Api},
 
-
-    {path: '/:pathmatch(.*)*', component: Http}, /* se puede hacer un componente de error */
+    {path: '/:pathmatch(.*)*', component: Api}, /* se puede hacer un componente de error */
 ]
 
 const router = createRouter({
